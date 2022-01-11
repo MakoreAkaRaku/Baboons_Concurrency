b0ec840f4f87498f278ca01cb2238de71b8c8f01
MakoreAkaRaku <marcromancolom@gmail.com>
2022-01-11 14:34:02 +0100

    random numbers added

--- src/Babuins_Viatgers.adb
+++ src/Babuins_Viatgers.adb
@@ -2,10 +2,11 @@ with Ada.Text_IO;
 use Ada.Text_IO;
 with Corda;
 use Corda;
-with Ada.Numerics.Discrete_Random;
-use Ada.Numerics.Discrete_Random;
+with Ada.Numerics.Float_Random;
+use Ada.Numerics.Float_Random;
 procedure Babuins_Viatgers is
-   type Babuins_Max_Delay is range 1.0..4.0;
+   Subtype Babuins_Max_Delay is Duration range 5.0..20.0;
+   Gen: Generator;
    Corda_Babuins : Def_Monitor;
    task type Babui_NORT is
       entry Start(Id: in Integer);
@@ -14,18 +15,19 @@ procedure Babuins_Viatgers is
    task body Babui_NORT is
       Orientacio : String := "Nord";
       Id_Babui_N : Integer;
+      Corda_Delay : Float;
    begin
       accept Start(Id: Integer) do
          Id_Babui_N := Id;
       end Start;
+      Corda_Delay := Random(Gen);
       Put_Line("Soc el babui "& Orientacio & Id_Babui_N'Img);
       
       for index in 1..3 loop
          Corda_Babuins.Cabo_Nord;
-         delay 0.5;
+         delay Duration(Corda_Delay);
          Corda_Babuins.Unlock_Nord;
-         
-         Put_Line("Babui "&Orientacio&Id_Babui_N'Img&": "&Index'Img&" de 3 voltes fetes");
+         Put_Line("Babui "&Orientacio&Id_Babui_N'Img&": "&Index'Img&" de 3 voltes fetes // he tardat "&Corda_Delay'Img&" de temps");
          delay 5.0;
       end loop;
       Put_Line("Babui " & Orientacio & Id_Babui_N'Img & ": He acabat!");
@@ -37,20 +39,22 @@ procedure Babuins_Viatgers is
    task body Babui_SUD is
       Orientacio : String := "Sud";
       Id_Babui_S : Integer;
-   begin
-      accept Start(Id: Integer) do
-         Id_Babui_S := Id;
-      end Start;
-      Put_Line(ASCII.HT&"Soc el babui "& Orientacio & Id_Babui_S'Img);
+      Corda_Delay : Float;
+      begin
+         accept Start(Id: Integer) do
+            Id_Babui_S := Id;
+         end Start;
+         Corda_Delay := Random(Gen);
+         Put_Line(ASCII.HT&"Soc el babui "& Orientacio & Id_Babui_S'Img);
       
-      for index in 1..3 loop
-         Corda_Babuins.Cabo_Sud;
-         delay 0.5;
-         Corda_Babuins.Unlock_Sud;
-         Put_Line(ASCII.HT&"Babui "&Orientacio&Id_Babui_S'Img&": "&Index'Img&" de 3 voltes fetes");
-         delay 5.0;
-      end loop;
-      Put_Line(ASCII.HT&"Babui "& Orientacio& Id_Babui_S'Img&": He acabat!");
+         for Index in 1..3 loop
+            Corda_Babuins.Cabo_Sud;
+            delay Duration(Corda_Delay);
+            Corda_Babuins.Unlock_Sud;
+            Put_Line(ASCII.HT&"Babui "&Orientacio&Id_Babui_S'Img&": "&Index'Img&" de 3 voltes fetes");
+            delay 5.0;
+         end loop;
+         Put_Line(ASCII.HT&"Babui "& Orientacio& Id_Babui_S'Img&": He acabat!");
    end Babui_SUD;
    
    
@@ -60,6 +64,7 @@ procedure Babuins_Viatgers is
    BabuinsSud : Babuins_S;
    BabuinsNord: Babuins_N;
 begin
+   Reset(Gen);
    for Index in 1..N_BABUINS loop
       BabuinsSud(Index).Start(Index);
       BabuinsNord(Index).Start(Index);
