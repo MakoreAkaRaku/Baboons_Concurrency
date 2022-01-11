9ccf08f81eb6b759f7d494c144a4300785832907
Andreas Manuel Korn <andreas.korn98@gmail.com>
2022-01-11 17:11:04 +0100

    Commented code and decoupled return speed from going speed

--- src/Babuins_Viatgers.adb
+++ src/Babuins_Viatgers.adb
@@ -5,10 +5,14 @@ use Corda;
 with Ada.Numerics.Float_Random;
 use Ada.Numerics.Float_Random;
 procedure Babuins_Viatgers is
-   Babuins_Max_Delay :Float := 15.0;
-   Babuins_Min_Delay :Float := 5.0;
+   -- Variables de les tasques
+   Babuins_Max_Delay: Float := 10.0; -- Delay màxim
+   Babuins_Min_Delay: Float := 1.0; -- Delay mínim
+   Delay_Tornada: constant Float := 7.5; -- Delay per tornar a l'origen
+   Nombre_De_Voltes: Integer := 3; -- Quàntes voltes ha de fer cada babui
    Gen: Generator;
    Corda_Babuins : Def_Monitor;
+   
    task type Babui_NORT is
       entry Start(Id: in Integer);
    end Babui_NORT;
@@ -19,17 +23,22 @@ procedure Babuins_Viatgers is
       Corda_Delay : Float;
    begin
       accept Start(Id: Integer) do
-         Id_Babui_N := Id;
+         Id_Babui_N := Id; -- L'identificador del babui
       end Start;
-      Corda_Delay := Random(Gen)*(Babuins_Max_Delay-Babuins_Min_Delay)+Babuins_Min_Delay;
+      
       Put_Line("Soc el babui "& Orientacio & Id_Babui_N'Img);
       
-      for index in 1..3 loop
+      for index in 1..Nombre_De_Voltes loop
+         -- Establim un delay aleatori entre el mínim i el màxim 
+         Corda_Delay := Random(Gen)*(Babuins_Max_Delay-Babuins_Min_Delay)+Babuins_Min_Delay;
+         -- Comença a creuar la corda
          Corda_Babuins.Cabo_Nord;
          delay Duration(Corda_Delay);
+         -- Acaba de creuar la corda
          Corda_Babuins.Unlock_Nord;
          Put_Line("Babui "&Orientacio&Id_Babui_N'Img&": "&Index'Img&" de 3 voltes fetes");
-         delay Duration(Babuins_Max_Delay);
+         -- Torna al ser origen
+         delay Duration(Delay_Tornada);
       end loop;
       Put_Line("Babui " & Orientacio & Id_Babui_N'Img & ": He acabat!");
    end Babui_NORT;
@@ -41,24 +50,29 @@ procedure Babuins_Viatgers is
       Orientacio : String := "Sud";
       Id_Babui_S : Integer;
       Corda_Delay : Float;
-      begin
-         accept Start(Id: Integer) do
-            Id_Babui_S := Id;
-         end Start;
-         Corda_Delay := Random(Gen)*(Babuins_Max_Delay-Babuins_Min_Delay)+Babuins_Min_Delay;
-         Put_Line(ASCII.HT&"Soc el babui "& Orientacio & Id_Babui_S'Img);
+   begin
+      accept Start(Id: Integer) do
+         Id_Babui_S := Id;
+      end Start;
       
-         for Index in 1..3 loop
-            Corda_Babuins.Cabo_Sud;
-            delay Duration(Corda_Delay);
-            Corda_Babuins.Unlock_Sud;
-            Put_Line(ASCII.HT&"Babui "&Orientacio&Id_Babui_S'Img&": "&Index'Img&" de 3 voltes fetes");
-            delay Duration(Babuins_Max_Delay);
-         end loop;
-         Put_Line(ASCII.HT&"Babui "& Orientacio& Id_Babui_S'Img&": He acabat!");
+      Put_Line(ASCII.HT&"Soc el babui "& Orientacio & Id_Babui_S'Img);
+      
+      for Index in 1..Nombre_De_Voltes loop
+         -- Establim un delay aleatori entre el mínim i el màxim
+         Corda_Delay := Random(Gen)*(Babuins_Max_Delay-Babuins_Min_Delay)+Babuins_Min_Delay;
+         -- Comença a creuar la corda
+         Corda_Babuins.Cabo_Sud;
+         delay Duration(Corda_Delay);
+         -- Acaba de creuar la corda
+         Corda_Babuins.Unlock_Sud;
+         Put_Line(ASCII.HT&"Babui "&Orientacio&Id_Babui_S'Img&": "&Index'Img&" de 3 voltes fetes");
+         -- Torna al ser origen
+         delay Duration(Delay_Tornada);
+      end loop;
+      Put_Line(ASCII.HT&"Babui "& Orientacio& Id_Babui_S'Img&": He acabat!");
    end Babui_SUD;
    
-   
+   -- Variables del main
    N_BABUINS: Integer:= 5;
    type Babuins_N is array (1..N_BABUINS) of Babui_NORT;
    type Babuins_S is array (1..N_BABUINS) of Babui_SUD;
@@ -67,6 +81,7 @@ procedure Babuins_Viatgers is
 begin
    Reset(Gen);
    for Index in 1..N_BABUINS loop
+      -- Inicialitzam tots el babuins
       BabuinsSud(Index).Start(Index);
       BabuinsNord(Index).Start(Index);
    end loop;
